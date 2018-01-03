43dfda663e7d80c2dd25cd7ebe122ae3fb6f1dcc
Vigneshwaran Ravimurugan <vigneshwaran.ravimurugan@kaaylabs.com>
2018-01-02 23:46:26 -0500

    webcrawler 1.0

---
+++ training.py
@@ -0,0 +1,21 @@
+print("welcome to python")
+
+how_many_snakes = 5
+snake_string = """
+
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Philip and Charlie
+
+"""
+
+#print(snake_string*how_many_snakes)
+sentence1 = "I wish to register a complaint."
+print(len(sentence1))
